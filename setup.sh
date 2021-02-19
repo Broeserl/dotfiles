cfe417903fa583c934106dd185ce927c7b51c35d
Johannes Juraske <broeserl1+gitlab@gmail.com>
2021-02-19 12:14:23 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,13 +5,19 @@ DEST_DIR="/home/$USER/"
 install_dev_tools() {
   echo "###################################################################"
   echo "\nInstall development tools\n"
-  sudo apt -y install git g++ gcc cmake python3.8-dev 
+  sudo apt -y install git g++ gcc cmake python3.8-dev valgrind grep
   echo "###################################################################"
 }
 
 install_std_tools() {
   echo "\nInstall standard tools\n"
-  sudo apt -y install curl wget vim screen
+  sudo apt -y install curl wget vim screen net-tools htop dos2unix
+  echo "###################################################################"
+}
+
+install_useful_tools() {
+  echo "\Install useful tools\n"
+  sudo apt -y install diodon filezilla flameshot gparted neofetch ser2net rclone virtualbox virtualbox-ext-pack virtualbox-dkms
   echo "###################################################################"
 }
 
