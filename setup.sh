eb2a8a1839c1e72e4cd1ba132b09877ba0474954
Broeserl <broeserl1@gmail.com>
2021-05-02 20:52:43 +0200

    Update setup.sh
    Removed stuff, which is not needed for docker

--- setup.sh
+++ setup.sh
@@ -5,19 +5,13 @@ DEST_DIR="/home/$USER/"
 install_dev_tools() {
   echo "###################################################################"
   echo "Install development tools"
-  sudo apt -y install git g++ gcc cmake python3.8-dev valgrind grep
+  apt -y install git g++ gcc cmake python3.8-dev valgrind grep
   echo "###################################################################"
 }
 
 install_std_tools() {
   echo "Install standard tools"
-  sudo apt -y install curl wget vim screen net-tools htop dos2unix
-  echo "###################################################################"
-}
-
-install_useful_tools() {
-  echo "Install useful tools"
-  sudo apt -y install diodon filezilla flameshot gparted neofetch ser2net tree rclone virtualbox virtualbox-ext-pack virtualbox-dkms
+  apt -y install curl wget vim screen net-tools htop dos2unix
   echo "###################################################################"
 }
 
@@ -31,13 +25,13 @@ install_ohmyzsh_plugins() {
 install_fonts() {
   wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Mononoki.zip
   unzip Mononoki.zip -d mononoki-fonts
-  sudo cp mononoki-fonts/*.ttf /usr/local/share/fonts
+  cp mononoki-fonts/*.ttf /usr/local/share/fonts
   rm -rf Mononoki.zip mononoki-fonts
 }
 
 install_ohmyzsh() {
   echo "Install zsh & oh_my_zsh"
-  sudo apt -y install zsh fzf python3-pip xclip 
+  apt -y install zsh fzf python3-pip xclip 
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
   chsh -s /usr/bin/zsh
   install_ohmyzsh_plugins
@@ -56,7 +50,6 @@ add_custom_config_to_bashrc() {
 
 install_dev_tools
 install_std_tools
-install_useful_tools
 install_ohmyzsh
 copy_config_files
 add_custom_config_to_bashrc
